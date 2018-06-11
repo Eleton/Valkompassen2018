4fae818d89937f1d268d1ac15c7524d76ee1b315
Eleton <eleton@gmail.com>
2018-06-11 22:01:11 +0200

    Styling

--- index.elm
+++ index.elm
@@ -38,7 +38,7 @@ type Ideology
 question1 : Question
 question1 =
   Question
-    "Vilken av följande två är viktigare än den andra?"
+    "Vad är det finaste som en stat kan ge dig?"
     (Answer Left "Trygghet")
     (Answer Right "Frihet")
 
@@ -48,6 +48,12 @@ question2 =
     "Vilket av följande personlighetsdrag anser du är osexigast hos en partner?"
     (Answer Left "Girighet")
     (Answer Right "Lathet")
+
+blue : String
+blue = "rgb(0, 106, 179)"
+
+red : String
+red = "rgb(237, 27, 52)"
 
 -- UPDATE
 
@@ -87,7 +93,9 @@ frontBox =
   div [ box ]
   [ Html.h1 [ h1Style ] [ text "Aimans Valkompass 2018" ]
   , Html.h2 [ h2Style ] [ text "Gör Aimans Valkompass 2018 och ta reda på vilket parti du borde rösta på i valet 2018!" ]
-  , button [ onClick Start, buttonStyle "green" ] [ text "Start" ]
+  , div [ boxContainer ]
+    [ button [ onClick Start, buttonStyle "green" ] [ text "Start" ]
+    ]
   ]
 
 resultBox : List Ideology -> Html Msg
@@ -95,7 +103,9 @@ resultBox result =
   div [ box ]
   [ Html.h1 [ h1Style ] [ text "Aimans Valkompass 2018" ]
   , Html.h2 [ h2Style ] [ text (resultToText result) ]
-  , button [ onClick Redo, buttonStyle "green" ] [ text "Gör om" ]
+  , div [ boxContainer ]
+    [ button [ onClick Redo, buttonStyle "green" ] [ text "Gör om" ]
+    ]
   ]
 
 questionBox : Question -> Html Msg
@@ -105,13 +115,14 @@ questionBox q =
   , Html.h2 [ h2Style ] [ text q.question ]
   , div [] [
     button
-      [ onClick (AddAnswer q.firstAnswer.ideology), buttonStyle "red" ]
+      [ onClick (AddAnswer q.firstAnswer.ideology), buttonStyle red ]
       [ text q.firstAnswer.text]
     , button
-      [ onClick (AddAnswer q.secondAnswer.ideology), buttonStyle "blue" ]
+      [ onClick (AddAnswer q.secondAnswer.ideology), buttonStyle blue ]
       [ text q.secondAnswer.text ]
     ]
   ]
+
 
 -- HELP FUNCTIONS
 
@@ -124,6 +135,8 @@ resultToText list =
       "Du är uppenbart genomblå! Aimans Valkompass föreslår därför att du lägger din röst på något av Allianspartierna."
     _ ->
       "Det är omöjligt att placera dig helt klockrent på Höger/Vänster-skalan. Du svajar helt enkelt för mycket i din ideologi. Statistiskt sett så borde du rösta på S, men M går också bra."
+
+
 -- STYLE
 
 container = 
@@ -147,7 +160,8 @@ box =
     , ("max-width", "800px")
     , ("height", "80vh")
     , ("padding", "20px")
-    , ("padding-bottom", "5vh")
+    , ("padding-top", "4vh")
+    , ("padding-bottom", "3vh")
     , ("background-color", "orange")
     , ("border", "1px solid black")
     , ("box-shadow", "0 0 50px black")
@@ -165,21 +179,30 @@ box =
 buttonStyle color =
   style
     [ ("font-size", "5vh")
-    , ("border", "none")
-    , ("background-color", color)
-    , ("color", "ivory")
-    , ("border-radius", "3px")
+    , ("border", "4px solid black")
+    , ("background-color", "ivory")
+    , ("border-radius", "8px")
     , ("margin", "15px")
+    , ("padding", "10px 20px")
+    , ("cursor", "pointer")
+    , ("min-width", "20vw")
+    , ("max-width", "40vw")
+    , ("font-family", "Gotham-Bold,sans-serif")
+    , ("box-shadow", "inset 0px 0px 0px 10px " ++ color)
     ]
 
 h1Style =
   style
-    [ ("font-size", "5.5vh")
+    [ ("font-size", "6vh")
     , ("flex", "1")
+    , ("font-family", "Publik,Helvetica,Arial,Nimbus Sans L,Bitstream Vera Sans,sans-serif")
+    , ("text-shadow", "2px 2px 20px ivory")
+    , ("margin", "0px")
     ]
 
 h2Style =
   style
-    [ ("font-size", "4vh")
+    [ ("font-size", "3.5vh")
     , ("flex", "5")
+    , ("font-family", "arial,sans-serif")
     ]
